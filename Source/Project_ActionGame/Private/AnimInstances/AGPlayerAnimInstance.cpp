2ea33c4efff4720618248627e34bf91707f729ff
Vasilios Moutafis <vmoutafis94@gmail.com>
2023-11-24 00:09:27 +1100

    Retargeted Anims and Added Basic Attack Combo 1\2

---
+++ Source/Project_ActionGame/Private/AnimInstances/AGPlayerAnimInstance.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimInstances/AGPlayerAnimInstance.h"
+
+#include "AGPlayerController.h"
+#include "Characters/AGPlayerCharacter.h"
+
+UAGPlayerAnimInstance::UAGPlayerAnimInstance()
+{
+	MovementInputVector = FVector2D();
+	PlayerRef = nullptr;
+}
+
+void UAGPlayerAnimInstance::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+
+	PlayerRef = Cast<AAGPlayerCharacter>(TryGetPawnOwner());
+}
+
+void UAGPlayerAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if (!IsValid(PlayerRef))
+		return;
+
+	MovementInputVector = Cast<AAGPlayerController>(PlayerRef->GetController())->GetMovementInputVector();
+}
+
+void UAGPlayerAnimInstance::AttackComplete() const
+{
+	if (!IsValid(PlayerRef))
+		return;
+
+	PlayerRef->AttackComplete();
+}
+
+void UAGPlayerAnimInstance::ResetCombo() const
+{
+	if (!IsValid(PlayerRef))
+		return;
+
+	PlayerRef->EndBasicAttackCombo();
+}
---
+++ Source/Project_ActionGame/Public/AnimInstances/AGPlayerAnimInstance.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AGAnimInstance.h"
+#include "AGPlayerAnimInstance.generated.h"
+
+class AAGPlayerCharacter;
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_ACTIONGAME_API UAGPlayerAnimInstance : public UAGAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UAGPlayerAnimInstance();
+
+	virtual void NativeBeginPlay() override;
+	
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UFUNCTION(BlueprintCallable, Category="Player")
+	void AttackComplete() const;
+
+	UFUNCTION(BlueprintCallable, Category="Player")
+	void ResetCombo() const;
+	
+protected:
+	UPROPERTY(BlueprintReadOnly, Category=Player)
+	AAGPlayerCharacter* PlayerRef;
+	
+	UPROPERTY(BlueprintReadOnly, Category=Player)
+	FVector2D MovementInputVector;
+	
+};
