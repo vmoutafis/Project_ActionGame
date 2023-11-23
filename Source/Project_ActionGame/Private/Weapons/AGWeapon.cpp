579ba4c971e791c762de19cd01ee5cb73bae9708
Vasilios Moutafis <vmoutafis94@gmail.com>
2023-11-24 10:10:16 +1100

    Weapons pack added + Weapons base class 2

---
+++ Source/Project_ActionGame/Private/Weapons/AGWeapon.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapons/AGWeapon.h"
+
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+AAGWeapon::AAGWeapon()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon Mesh"));
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+	DamageCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("Damage Collision"));
+	DamageCollision->SetBoxExtent(FVector(50.0f, 50.0f, 50.0f));
+	DamageCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AAGWeapon::InitialiseWeapon(AActor* ParentOwner, const float& WeaponDamage)
+{
+	SetOwner(ParentOwner);
+	Damage = WeaponDamage;
+}
+
+void AAGWeapon::ActivateDamage()
+{
+	DamageCollision->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+}
+
+void AAGWeapon::DeactivateDamage()
+{
+	DamageCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+// Called when the game starts or when spawned
+void AAGWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+
+	DamageCollision->OnComponentBeginOverlap.AddDynamic(this, &AAGWeapon::OnDamageColliderHit);
+}
+
+void AAGWeapon::OnDamageColliderHit(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor == GetOwner())
+		return;
+
+	AController* OwnerController = Cast<APawn>(GetOwner())->GetController();
+	
+	UGameplayStatics::ApplyDamage(OtherActor, Damage, OwnerController, this, DamageType);
+}
---
+++ Source/Project_ActionGame/Public/Weapons/AGWeapon.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AGWeapon.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class PROJECT_ACTIONGAME_API AAGWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AAGWeapon();
+
+	void InitialiseWeapon(AActor* ParentOwner, const float& WeaponDamage);
+
+	// Turn on the damage collider for the weapon
+	void ActivateDamage();
+
+	// Turn off the damage collider for the weapon
+	void DeactivateDamage();
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void OnDamageColliderHit(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Weapon)
+	USkeletalMeshComponent* WeaponMesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Weapon)
+	UBoxComponent* DamageCollision;
+
+protected:
+	float Damage;
+
+	UPROPERTY(EditDefaultsOnly, Category="Weapon|DamageType")
+	TSubclassOf<UDamageType> DamageType;
+	
+};
