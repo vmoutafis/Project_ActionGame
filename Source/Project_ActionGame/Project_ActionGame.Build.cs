bb8af4bcf8ecf40adbbe677f3ea0d7d8f56b50d1
Vasilios Moutafis <vmoutafis94@gmail.com>
2024-10-16 23:58:08 +1100

    fixed packagaing

--- Source/Project_ActionGame/Project_ActionGame.Build.cs
+++ Source/Project_ActionGame/Project_ActionGame.Build.cs
@@ -6,7 +6,7 @@ public class Project_ActionGame : ModuleRules
 {
 	public Project_ActionGame(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PrivateDependencyModuleNames.AddRange(new string[] { "DerivedDataCache", "AnimGraphRuntime", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+		PrivateDependencyModuleNames.AddRange(new string[] {"AnimGraphRuntime", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Slate", "SlateCore", "AIModule" });
